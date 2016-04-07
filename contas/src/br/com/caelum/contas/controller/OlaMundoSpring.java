48a2985b7281c29a11d78f711a6345442b522f54
Marcos Barbosa <malexandrebarbo@gmail.com>
2016-04-07 18:34:07 -0300

    Executando minha primeira lógia: Olá Mundo Spring

---
+++ contas/src/br/com/caelum/contas/controller/OlaMundoSpring.java
@@ -0,0 +1,15 @@
+package br.com.caelum.contas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OlaMundoSpring {
+	
+	@RequestMapping("/olaMundoSpring")
+	public String execute() {
+		System.out.println("Executando minha primeira lógica!");
+		return "ok";
+	}
+
+}
