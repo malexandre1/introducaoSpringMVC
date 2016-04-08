00cbd700c5b9dd77498c049343700cb532b80442
Marcos Barbosa <malexandrebarbo@gmail.com>
2016-04-07 23:21:44 -0300

    Criando o formulario.jsp, criando a action formulario, criando o conta-adicionada.jsp, convertendo e mapeando o ENUM.

--- contas/src/br/com/caelum/contas/controller/OlaMundoSpring.java
+++ contas/src/br/com/caelum/contas/controller/OlaMundoSpring.java
@@ -3,6 +3,9 @@ package br.com.caelum.contas.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.caelum.contas.dao.ContaDAO;
+import br.com.caelum.contas.modelo.Conta;
+
 @Controller
 public class OlaMundoSpring {
 	
@@ -10,6 +13,19 @@ public class OlaMundoSpring {
 	public String execute() {
 		System.out.println("Executando minha primeira lógica!");
 		return "ok";
+	}
+	
+	@RequestMapping("/adicionaConta")
+	public String adiciona(Conta conta) {
+		ContaDAO dao = new ContaDAO();
+		dao.adiciona(conta);
+		
+		return "conta/conta-adicionada";
+	}
+	
+	@RequestMapping("/form")
+	public String form() {
+		return "conta/formulario";
 	}
 
 }
