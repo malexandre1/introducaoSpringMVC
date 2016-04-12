5defaa646576191c16c5341c505f6c7e1453b795
Marcos Barbosa <malexandrebarbo@gmail.com>
2016-04-12 10:56:41 -0300

    Validando dados com Bean Validation - campo descrição

--- contas/src/br/com/caelum/contas/controller/ContaController.java
+++ contas/src/br/com/caelum/contas/controller/ContaController.java
@@ -2,8 +2,11 @@ package br.com.caelum.contas.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.caelum.contas.dao.ContaDAO;
@@ -13,7 +16,11 @@ import br.com.caelum.contas.modelo.Conta;
 public class ContaController {
 	
 	@RequestMapping("/adicionaConta")
-	public String adiciona(Conta conta) {
+	public String adiciona(@Valid Conta conta, BindingResult result) {
+		if (result.hasFieldErrors("descricao")) {
+			return "conta/formulario";
+		}
+		
 		ContaDAO dao = new ContaDAO();
 		dao.adiciona(conta);
 		
@@ -51,7 +58,7 @@ public class ContaController {
 		ContaDAO dao = new ContaDAO();
 		dao.remove(conta);
 		
-		return "forward:listaContas";
+		return "redirect:listaContas";
 	}
 	
 	@RequestMapping("mostraConta")
